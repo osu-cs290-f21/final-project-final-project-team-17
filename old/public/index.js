279ab09137e3223621cc273fd5cb3ec8a5bda1f8
YuhangZhang2 <zhanyuh2@oregonstate.edu>
2021-12-09 19:28:04 -0800

    12/9

---
+++ old/public/index.js
@@ -0,0 +1,86 @@
+/* function emulating insertNewPost from assignment 5; meant to insert new recipe
+ into index html*/
+
+ 
+function insertNewRecipe(name, cooktime, imageURL,instructions){
+
+    var newRecipeContent = {
+        "name": name,
+        "cooktime": cooktime,
+        "imageURL": imageURL,
+        "instructions": instructions
+    }
+
+    var newRecipeHTML = Handlebars.templates.newRecipe(newRecipeContent)
+
+    var recipeContainer = document.getElementsByClassName('recipes')
+    recipeContainer.insertAdjacentHTML('beforeend', newRecipeHTML)
+}
+
+function backToMainPage(){
+  location.href = "http://localhost:3000/"      //how to not hard code this
+}
+
+var cancelInputButton = document.getElementById("modal-cancel")
+if(cancelInputButton){
+  cancelInputButton.addEventListener("click", backToMainPage)
+}
+
+var closeInputButton = document.getElementById("modal-close")
+if(closeInputButton){
+  closeInputButton.addEventListener("click", backToMainPage)
+}
+
+var headerClick = document.getElementById("headTitle")
+if(headerClick){
+  headerClick.addEventListener("click", backToMainPage)
+}
+
+function handleModalAcceptClick() {      //not working
+
+  var photoURLNew = document.getElementById('recipe-photo-input').value.trim();
+  var instructionsNew = document.getElementById('recipe-instructions-input').value.trim();
+  var nameNew = document.getElementById('recipe-name-input').value;
+  var cooktimeNew = document.getElementById('recipe-cooktime-input').value;
+
+  if (!photoURLNew || !instructionsNew ||!nameNew ||! cooktimeNew) {
+    alert("You must fill in all of the fields!");
+  } else {
+    //this code is what is cited by the browser as not working when we try to make a new recipe
+    var req = new XMLHttpRequest()
+    var url = '/recipe/2' //we know that this is wrong for a matter of fact
+    console.log("== url:", url)
+    req.open('POST', url)
+    
+    var recipeObj = {
+      name: nameNew,
+      cooktime: cooktimeNew,
+      photoURL: photoURLNew,
+      instructions: instructionsNew
+    }
+    var reqBody = JSON.stringify(recipeObj)
+    console.log("== reqBody:", reqBody)
+
+    req.addEventListener('load', function (event) {
+      if (event.target.status === 200) {
+        var recipesTemplate = Handlebars.templates.newRecipe;
+        var newRecipeHTML = recipesTemplate(recipeObj);
+        var recipeContainer = document.querySelector('.recipes');
+        recipeContainer.insertAdjacentHTML('beforeend', newRecipeHTML);
+      } else {
+        alert("Error saving recipe: " + event.target.response)
+      }
+    })
+    req.setRequestHeader('Content-Type', 'application/json')
+    req.send(reqBody)
+    console.log(reqBody)
+   
+
+  }
+
+}
+
+var addrecipeButtons = document.getElementById('modal-accept')
+console.log(addrecipeButtons)
+addrecipeButtons.addEventListener('click', handleModalAcceptClick)
+//complete the rest later
---
+++ old/server.js
@@ -0,0 +1,46 @@
+var path = require('path')
+var express = require('express')
+var exphbs = require('express-handlebars')
+var recipeData = require('./recipeData.json')
+var app = express()
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res, next){
+    res.status(200).render('indexPage', {
+        indexPage: true,
+        recipeData
+    })
+    console.log("passed the / render")
+    console.log ("data", recipeData)
+})
+
+app.get('/recipes/:recipeArr', function(req, res, next){
+    var recipeArr = req.params.recipeArr;
+    console.log('-- recipeArr: ', recipeArr)
+    if(recipeData[recipeArr]){
+        res.status(200).render('recipePage', recipeData[recipeArr])
+    }
+    else{
+        next();
+    }
+})
+app.get('/creating', function (req, res, next){
+  res.status(200).render('creatingRecipe');
+})
+
+app.get('/about', function (req, res){
+    res.status(200).render('aboutus');
+  })
+
+app.get('*', function(req, res){
+    res.status(404).render('404Page')
+})
+
+app.listen(port, function () {
+    console.log("== Server is listening on port: ", port);
+  });
